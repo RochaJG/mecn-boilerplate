3ab5656ed53e8164ccc066c64a39e7c65e5e509f
Jordan Rocha <70rd23@gmail.com>
2020-08-18 16:15:08 -0300

    Added basic server with custom logger

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('MECN Boilerplate');
+const app = require('./app');
+const config = require('./config');
+const { logger } = require('./libs');
+
+app.listen(config.port, () => {
+  logger.info(`Server listening to port ${config.port}`);
+});
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      logger.info('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM received');
+  if (server) {
+    server.close();
+  }
+});
